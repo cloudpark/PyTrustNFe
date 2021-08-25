29e9619d2ae469906161e3e8e44ad3a77fe8f4d7
rkoball <ricardo.koball@gmail.com>
2021-08-25 15:30:26 -0300

    Consulta e Exportação de lote de RPS Blumenau

--- pytrustnfe/nfse/simplissbnu/__init__.py
+++ pytrustnfe/nfse/simplissbnu/__init__.py
@@ -3,12 +3,15 @@ import os
 import requests
 
 from lxml import etree
-from pytrustnfe.xml import render_xml
+from pytrustnfe.xml import render_xml, sanitize_response
 from pytrustnfe.certificado import extract_cert_and_key_from_pfx, save_cert_key
 from pytrustnfe.nfse.assinatura import Signer
 
 URL = 'https://wsblumenau1.simplissweb.com.br/nfseservice.svc'
-HEADERS = {'Content-Type': 'text/xml; charset=utf-8', 'SOAPAction': "http://nfse.abrasf.org.br/INfseService/GerarNfse"}
+
+
+def _get_headers(method):
+    return {'Content-Type': 'text/xml; charset=utf-8', 'SOAPAction': "http://nfse.abrasf.org.br/INfseService/" + method}
 
 
 def _render_xml(method, **kwargs):
@@ -24,35 +27,67 @@ def _send(certificate, method, retry=0, **kwargs):
         cert_filename, key_filename = save_cert_key(cert_content, key_content)
 
         path = os.path.join(os.path.dirname(__file__), 'templates')
-        body = render_xml(path, '%s.xml' % method, False, **kwargs)
-        body = Signer().sign_xml(body, 'L1', cert_content, key_content)
+        body = render_xml(path, '%s.xml' % method, False, **kwargs).decode("utf-8")
+        print(body)
+        if method in ["GerarNfse", "RecepcionarLoteRps"]:
+            signer = Signer()
+            if method == "RecepcionarLoteRps":
+                for rps in kwargs["nfse"]["lista_rps"]:
+                    print(rps)
+                    body_rps = render_xml(path, 'Rps.xml', False, **rps)
+                    print(body_rps)
+                    body_rps_signed = signer.sign_xml(body_rps, f'rps{rps["numero"]}', cert_content, key_content)
+                    print(body_rps_signed)
+                    print("--------------------------------")
+                    print(body)
+                    body = body.replace(f"rps_{rps['numero']}_rps", body_rps_signed)
+                    print(body)
 
-        data = '<?xml version="1.0" encoding="UTF-8"?>' \
+            body = signer.sign_xml(body.encode('utf-8'), 'L1', cert_content, key_content)
+#        else:
+#            body = body.decode("utf-8")
+
+        data = f'<?xml version="1.0" encoding="UTF-8"?>' \
                '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" ' \
                'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' \
                'xmlns:ns0="http://schemas.xmlsoap.org/soap/envelope/" ' \
                'xmlns:ns1="http://nfse.abrasf.org.br">' \
                '<SOAP-ENV:Header/>' \
-               '<ns0:Body><ns1:GerarNfseRequest>' \
-               '<nfseCabecMsg><![CDATA[<cabecalho versao="2.03" ' \
-               'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' \
-               'xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://www.abrasf.org.br/nfse.xsd">' \
-               '<versaoDados>2.03</versaoDados></cabecalho>]]>' \
-               '</nfseCabecMsg><nfseDadosMsg><![CDATA[%s]]></nfseDadosMsg></ns1:GerarNfseRequest></ns0:Body>' \
-               '</SOAP-ENV:Envelope>' % body
+               '<ns0:Body><ns1:' + method + 'Request>' \
+                                            '<nfseCabecMsg><![CDATA[<cabecalho versao="2.03" ' \
+                                            'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' \
+                                            'xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://www.abrasf.org.br/nfse.xsd">' \
+                                            '<versaoDados>2.03</versaoDados></cabecalho>]]>' \
+                                            '</nfseCabecMsg><nfseDadosMsg><![CDATA[' + body + ']]></nfseDadosMsg></ns1:' + method + 'Request></ns0:Body>' \
+                                                                                                                                    '</SOAP-ENV:Envelope>'
         data = data.encode()
 
+        print(data)
         session = requests.Session()
         session.cert = (cert_filename, key_filename)
-        response = session.post(URL, data=data, headers=HEADERS)
+        response = session.post(URL, data=data, headers=_get_headers(method))
         print(response.status_code)
-        print(response.text)
         if response.status_code != 200 or "E900" in response.text:
-            if retry <= 2:
-                retry = retry + 1
-                _send(certificate, method, retry, **kwargs)
-            else:
-                raise Exception("Erro ao exportar a RPS. %d - %s" % (response.status_code, response.text))
+            error = "Erro ao exportar a RPS. %d - %s" % (response.status_code, response.text)
+            print(error)
+            return {
+                'sent_xml': data,
+                'received_xml': error,
+                'object': None
+            }
+        else:
+            response, obj = sanitize_response(response.text)
+            if method == "RecepcionarLoteRps":
+                response, obj = sanitize_response(str(obj.Body.RecepcionarLoteRpsResponse['outputXML']))
+            elif method == "ConsultarLoteRps":
+                response, obj = sanitize_response(str(obj.Body.ConsultarLoteRpsResponse['outputXML']))
+            elif method == "GerarNfse":
+                response, obj = sanitize_response(str(obj.Body.GerarNfseResponse['outputXML']))
+            return {
+                'sent_xml': data,
+                'received_xml': response,
+                'object': obj
+            }
     except Exception as e:
         print(e)
         raise e
@@ -64,17 +99,13 @@ def gerar_nfse(certificate, **kwargs):
     return _send(certificate, 'GerarNfse', 0, **kwargs)
 
 
-def consultar_lote_rps(certificate, **kwargs):
+def recepcionar_lote_rps(certificado, **kwargs):
+    if "xml" not in kwargs:
+        kwargs['xml'] = _render_xml('RecepcionarLoteRps', **kwargs)
+    return _send(certificado, 'RecepcionarLoteRps', 0, **kwargs)
+
+
+def consultar_lote_rps(certificado, **kwargs):
     if "xml" not in kwargs:
         kwargs['xml'] = _render_xml('ConsultarLoteRps', **kwargs)
-    return _send(certificate, 'ConsultarLoteRps', 0, **kwargs)
-
-
-def xml_cancelar_nfse(**kwargs):
-    return _render_xml('CancelarNfse', **kwargs)
-
-
-def cancelar_nfse(certificado, **kwargs):
-    if "xml" not in kwargs:
-        kwargs['xml'] = xml_cancelar_nfse(**kwargs)
-    return _send(certificado, 'CancelarNfse', **kwargs)
+    return _send(certificado, 'ConsultarLoteRps', 2, **kwargs)
